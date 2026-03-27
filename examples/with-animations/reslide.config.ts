966dbdde0822a9080eb345b3894adcae1e1dda85
Dohyeon Dodo Park <dodo41142727@gmail.com>
2026-03-28 05:21:16 +0900

    chore(examples): remove conference-talk and with-animations examples

--- examples/with-animations/reslide.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from '@reslide/core';
-
-export default defineConfig({
-  title: 'Animations Example',
-  transition: { type: 'fade', duration: 400 },
-});
--- examples/with-animations/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-import { createRoot } from 'react-dom/client';
-import { Deck } from '@reslide/core';
-import { slides } from 'virtual:reslide/slides';
-import config from 'virtual:reslide/config';
-
-createRoot(document.getElementById('root')!).render(
-  <Deck slides={slides} config={config} />,
-);
